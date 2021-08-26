d1c040ddac4cc56f43d982c50251668a1ad1d334
jonathanothoudt <74217313+jothoudt@users.noreply.github.com>
2021-08-25 22:54:59 -0500

    display tasks component conditionally render tasks

--- src/components/DisplayTasks/DisplayTasks.jsx
+++ src/components/DisplayTasks/DisplayTasks.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import {useSelector} from 'react-redux';
 
 function DisplayTasks(){
+    const tasks= useSelector((store)=>{return store.list})
+
+    const displayAllTasks=()=>{
+        let display=''
+        if(!tasks.length){
+            display= <h2>No tasks</h2>
+        }
+        else{
+            display=
+            <>
+            {tasks.map((task, id)=>{
+                return(
+                    <DisplayTasks task={task} key={id} />
+                )
+            })}
+            </>
+        }
+    }
     return(
         <div>
-            
+            {displayAllTasks()}
         </div>
     )
 }

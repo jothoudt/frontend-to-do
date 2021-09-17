34709db6edb971349285249e074ae2eb53226a04
jonathanothoudt <74217313+jothoudt@users.noreply.github.com>
2021-09-16 20:47:58 -0500

    change styling

--- src/components/DisplayTasks/DisplayTasks.jsx
+++ src/components/DisplayTasks/DisplayTasks.jsx
@@ -1,6 +1,13 @@
 import React from 'react'
 import {useSelector} from 'react-redux';
 import DisplayTasksEach from '../DisplayTasksEach/DisplayTasksEach';
+//material ui
+import Box from '@material-ui/core/Box';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 
 function DisplayTasks({tasks}){
     //function to display tasks
@@ -22,20 +29,22 @@ function DisplayTasks({tasks}){
         return display;
     }
     return(
-        <div>
-            <table>
-                <tr>
-                    <th>Task</th>
-                    <th>Date Added</th>
-                    <th>Completed?</th>
-                    <th>Date Completed</th>
-                </tr>
-                <tr>
-                {displayAllTasks()}
-                </tr>
-            </table>
-            
-        </div>
+        <Box border={1} borderColor="#1E3163" style={{marginLeft:"auto", marginRight:"auto", width:"80%", marginTop:'50px'}}>
+            <Table>
+                <TableHead style={{backgroundColor:"#1E3163"}}>
+                    <TableRow border={1} borderColor="#1E3163">
+                        <TableCell align="right" style={{color:"#f8f8f8"}}>Task</TableCell>
+                        <TableCell align="right" style={{color:"#f8f8f8"}}>Date Added</TableCell>
+                        <TableCell align="right" style={{color:"#f8f8f8"}}>Completed?</TableCell>
+                        <TableCell align="right" style={{color:"#f8f8f8"}}>Date Completed</TableCell>
+                        <TableCell align="right" style={{color:"#f8f8f8"}}>Delete Task</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {displayAllTasks()}
+                </TableBody>
+            </Table>         
+        </Box>
     )
 }
 
--- src/components/DisplayTasksEach/DisplayTasksEach.jsx
+++ src/components/DisplayTasksEach/DisplayTasksEach.jsx
@@ -1,19 +1,31 @@
 import React from 'react';
 import {useDispatch} from 'react-redux';
+//material ui
+import Button from '@material-ui/core/Button';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
 
 function DisplayTasksEach({task}){
     const dispatch= useDispatch()
     const completeTask =()=>{
         dispatch({type:"COMPLETE_TASK", payload:task.id})
     }
-    let formattedDate = task.date_completed.slice(0,16)
+
+    let formattedDate;
+    const dateFormat=()=>{
+        if(task.date_completed){
+            formattedDate = task.date_completed.slice(0,16)
+        }
+        return formattedDate;
+    }
     return(
-        <>
-            <td>{task.task}</td>
-            <td>{task.date_added}</td>
-            {task.completed ? <td>&#10003;</td> : <td><button onClick={completeTask}>Complete</button></td>}
-            {task.date_completed === null ? <td>Not yet completed</td> : <td>{formattedDate}</td>}
-        </>
+        <TableRow style={ task.completed ? {backgroundColor:'#F037A5'} : {backgroundColor:'#F8F8F8'}}>
+            <TableCell align="right">{task.task}</TableCell>
+            <TableCell align="right">{task.date_added}</TableCell>
+            {task.completed ? <TableCell align="right">&#10003;</TableCell> : <TableCell align="right"><Button onClick={completeTask}>Complete</Button></TableCell>}
+            {task.date_completed === null ? <TableCell align="right" >Not yet completed</TableCell> : <TableCell align="right">{dateFormat()}</TableCell>}
+            <TableCell align="right"></TableCell>
+        </TableRow>
     )
 }
 

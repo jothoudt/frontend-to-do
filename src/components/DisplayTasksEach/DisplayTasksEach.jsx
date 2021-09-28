09bd95c139ef52ad52aa6ebecbba52828935a2eb
jonathanothoudt <74217313+jothoudt@users.noreply.github.com>
2021-09-28 17:31:41 -0500

    fix some formatting

--- src/components/DisplayTasksEach/DisplayTasksEach.jsx
+++ src/components/DisplayTasksEach/DisplayTasksEach.jsx
@@ -19,12 +19,38 @@ function DisplayTasksEach({task}){
         //send id of the task to be deleted
         dispatch({type:"DELETE_TASK", payload:task.id})
     }   //end deleteThisTask
+    //format date for date_added. *This is needed for the java backend version
+    const dateAddedFormat=()=>{
+        //define formatted date_added
+        let formattedDateAdded=''
+        let thisDate= task.date_added
+        // if there are no tasks or tasks haven't loaded yet
+        if(!thisDate){
+            formattedDateAdded=''
+        }   //end if 
+        //checks how long the string is. if it is too long slice it.
+        //PYTHON version is already formatted response from the server and the length is 29
+        else if(thisDate.length === 29 && thisDate[28] ==='T'){
+            formattedDateAdded= thisDate.slice(0.16)
+        }
+        else {
+            formattedDateAdded= thisDate.slice(0,10)
+        }   //end else
+        return formattedDateAdded;
+    }
     //define formatteddate
     let formattedDate;
     //function to make timestamp look nicer
-    const dateFormat=()=>{
+    const dateCompleteFormat=()=>{
+        let completeDate=task.date_completed;
+        if(!completeDate){
+            formattedDate=''
+        }
+        else if(completeDate.length === 18){
+            formattedDate=task.date_completed;
+        }
         //if the task is completed give the formatted version of the completed time.
-        if(task.date_completed){
+        else if(task.date_completed){
             formattedDate = task.date_completed.slice(0,16)
         }
         return formattedDate;
@@ -38,7 +64,7 @@ function DisplayTasksEach({task}){
             </TableCell>
             <TableCell style={ task.completed ? 
                 {textDecoration:'line-through',fontSize:"large", fontFamily:'Courgette, cursive'} : 
-                {textDecoration:'none',fontSize:"large", fontFamily:'Courgette, cursive'}}  align="right">{task.date_added}
+                {textDecoration:'none',fontSize:"large", fontFamily:'Courgette, cursive'}}  align="right">{dateAddedFormat()}
             </TableCell>
             {task.completed ? 
                 <TableCell style={{color:'green', fontSize:"36px"}} align="right">&#10003;</TableCell> : 
@@ -48,7 +74,7 @@ function DisplayTasksEach({task}){
             }
             {task.date_completed === null ? 
                 <TableCell style={{fontSize:"large", fontFamily:'Courgette, cursive'}} align="right" >Not yet completed</TableCell> : 
-                <TableCell align="right">{dateFormat()}</TableCell>
+                <TableCell style={{fontSize:"large", fontFamily:'Courgette, cursive'}}align="right">{dateCompleteFormat()}</TableCell>
             }
             <TableCell align="right"> <DeleteForeverIcon onClick={deleteThisTask} style={{fontSize:"36px"}} /> </TableCell>
         </TableRow>
